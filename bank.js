ad3a254ea0db599565fadc3cba74b2f593372d41
Brett Rogowski <BARogowski@gmail.com>
2016-08-17 20:16:41 -0400

    bank.js, with balanceToCoin algorithm

--- bank.js
+++ bank.js
@@ -5,34 +5,33 @@ Array.prototype.sum = function(sumThis, _start) {
   }, start);
 };
 
-var exchange = function() {
-  return {
-    copper: 10,
-    silver: 50,
-    gold:   100
-  };
+// in order for a reason!
+var exchange = {
+  gold:   100.00,
+  silver: 50.00,
+  copper: 10.00
 };
 
-var balanceToCoin = function(balance) {
-  var coins = [],
-  total     = 0;
-  [100, 50, 10].forEach(function(coin) {
-    while (total + coin <= balance) {
-      coins.push(coin);
-      total += coin;
-    }
-  });
-  return coins.reduce(function(acc, curr) {
-    if (typeof acc[curr] == 'undefined') {
-      acc[curr] = 1;
-    } else {
-      acc[curr] += 1;
-    }
-    return acc;
-  },
-  {});
+var newObj = function(key, val) { 
+    var obj = {};
+    obj[key] = val;
+    return obj;
 };
-console.log(balanceToCoin(23890));
+
+var balanceToCoin = function(total) {
+  var balance = parseFloat(total);
+  var change = Object.keys(exchange).map(function(key) {
+    var coin = exchange[key];
+    var remainder = balance % coin;
+    var units = balance - remainder;
+    balance = remainder;
+    return Math.floor(units / coin);
+  });
+  return Object.keys(exchange).map(function(key, index) {
+    return newObj(key, change[index]);
+  });
+};
+console.log(balanceToCoin(890));
 //needs to return gold:, silver:, coppers
 
 
@@ -52,6 +51,7 @@ var purse = function(options) {
   };
 
   var balance = function() {
+    //I edited this probably not the right way 
     var startingBalance = 0;
     return startingBalance - transactions.reduce(function(acc, transaction) {
       return transaction.price + acc;
