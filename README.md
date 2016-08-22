583a26561349297c75f15c669bb51c4663ef9e2c
Brett Rogowski <BARogowski@gmail.com>
2016-08-22 15:11:48 -0400

    attempt to create many rooms, edited readme with todo list'

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Adventure Game!
 
 This is a group built, multiplayer game using Node and JavaScript.
+
+player starts with a wheel of cheese and 500 gold, find the beggar and give him the cheese he gives you an amulet that opens the gates at symphony bay across the orchestral sea, where you will encounter the badass WAGNER, but you need to get the 'heavy metal axe' to win the fight, which is sold in the market
+
+TODO 
+map out:
+alley- add beggar
+market shopkeeper that sells the weapon to defeat Wagner the wicked  
+docks
+ship
+sea x 3 
+symphony bay
+mooring
+beach- 'you take the dingy to the beach'
+waterfall-show the amulet unlocks the PIT  
+orchestra PIT- appears wagner the most hated composer and you fight to the DEATH 
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 (function() {
+  var Room      = require('./lib/types/room');
   var Player    = require('./lib/types/player');
   var Weapon    = require('./lib/types/weapon');
   var Item      = require('./lib/types/item');
@@ -11,19 +12,27 @@
   var apple       = worldData.items[0];
   var moldyCheese = worldData.items[1];
 
+//var docks  = new Room ({name: 'docks', description: 'a massive shipyard with battleships and trading vessels', players:[], exits: [{'s':4, 'n':1}]});
+//var ship  = new Room({name:'ship', description:'The Tri-tone, a scary looking vessle, with old cannons and a little dingy', players:[], exits:[{'s':5,'n':3}]});
+//var orcSea  = new Room({name:'The Orchestral Sea', description:'A turbulent sea that seems to go on forever', players:[], exits:[{'e':6,'n':4}]});
+//var orcSea2  = new Room({name:'The Orchestral Sea', description:'A turbulent sea that seems to go on forever', players:[], exits:[{'e':7,'w':5}]});
+//var orcSea3  = new Room({name:'The Orchestral Sea', description:'A turbulent sea that seems to go on forever', players:[], exits:[{'e':8,'w':6}]});
+//var SymBay  = new Room({name:'Symphony Bay', description:'A calm bay, near what seems to be a deserted island',  players:[],exits:[{'w':9,'s':7}]});
+//var mooring  = new Room({name:'Mooring', description:'An old mooring, what the hell is that doing here?', players:[], exits:[{'s':10,'e':8}]});
+//var beach  = new Room({name:'beach', description:'a white sand beach, with an old beaten path that leads into the tropical forest', players:[], exits:[{'w':11,'n':9}]});
+//var waterfall  = new Room({name:'waterfall', description:'a massive gleaming waterfall that seems to fall form the heavens!', players:[], exits:[{'s':12,'e':10}]});
+//var orcPit  = new Room({name:'Orchestral Pit', description:'You walk through the waterfall, the earth gives way and you fall into the ORCHESTRAL PIT OF DISPAIR, where you are confronted by the egomaniac Wagner!', players:[], exits:[{'n':10}]});
+
+//worldData.rooms.push(docks);
+worldData.rooms.push(ship);
+
   p1.mainHand = worldData.weapons[2];
   p2.mainHand = worldData.weapons[1];
 
 //to add items, etc
   //var moldyCheese = new Item ({name: 'moldy cheese', description: 'festering fontina', flag: ['consumable'], health: -8});
   //worldData.items.push(moldyCheese);
-  alley.enter(p1);
-  
-//How do we accumulate and save health?
 
-  console.log(p1.attributes.health);
-  console.log(p1.eat(moldyCheese));
-  console.log(p1.attributes.health);
   Db().save();
 }());
 
